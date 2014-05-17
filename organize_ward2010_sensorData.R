53a97333c5e50c2b919ab6f7c7f3293451c79db9
Ryan Batt <battrd@gmail.com>
2014-05-16 21:25:05 -0500

    new read-in sonde data files

---
+++ organize_ward2010_sensorData.R
@@ -0,0 +1,217 @@
+
+
+ciClasses <- c("character","numeric", "character", rep("numeric",3), rep("integer",5), rep("numeric",2))
+calInfo <- read.table("/Users/Battrd/Documents/School&Work/WiscResearch/Metabolism/WardSondes/WardCalData.csv", sep=",", header=TRUE, colClasses=ciClasses)
+
+sites <- unique(calInfo[,"Site"])
+
+# =============
+# = Functions =
+# =============
+Sat2Conc <- function(SDO2sat, SDtemp){ # convert DO % sat to mg/L
+	SDO2mgL=(SDO2sat/100)*(-0.00000002057759*SDtemp^5+0.000002672016*SDtemp^4+(-0.0001884085)*SDtemp^3+0.009778012*SDtemp^2+(-0.4147241)*SDtemp+14.621)
+	return(SDO2mgL)
+}
+a.nc <- function(x) as.numeric(as.character(x)) # function to convert factor to numeric, but safe if starting as character, numeric, integer
+
+# =======================
+# = round.time Function =
+# =======================
+round.time <- function(x, units, input.format=NULL, output.format="%Y-%m-%d %H:%M:%S"){
+	# x = head(t.sonde0.na2[,"date"], 20) + 120
+	# units = "df.345 min"
+	# Check for invalid input classes
+	stopifnot(is.character(units)&is.character(output.format)&(is.null(input.format)|is.character(input.format)))
+	
+	# Determine time unit
+	unit.choices <- c("sec", "min", "hour", "day")
+	choices.or <- paste(unit.choices, collapse="|")
+	unit.pattern <- paste(".*(", choices.or, ").*", sep="")
+	unit <- gsub(unit.pattern, "\\1", units)
+	if(is.na(unit)){stop("not a valid unit, use sec, min, hour, or day")}
+	which.choice <- which(unit==unit.choices)
+	
+	# Determine time interval
+	u.time.pattern <- "(?:[0-9]+\\.[0-9]+)|(?:[0-9]+\\.)|(?:\\.[0-9]+)|(?:[0-9]+)"
+	u.time.char <- regmatches(units, regexpr(u.time.pattern, units, perl=TRUE))
+	u.time <- as.numeric(u.time.char)
+	u.time <- ifelse(is.na(u.time), 1, u.time)
+	
+	unit.cutoff <- switch(unit, sec=60, min=60, hour=24, day=1)
+	
+	# =========================================================================
+	# = Check for invalid input (before slow [attempted] conversion to POSIX) =
+	# =========================================================================
+	if(sign(u.time)==-1L){
+		stop("time interval must be positive")
+	}
+	# Deal with case where units are 1 second (or less)
+	if(unit=="sec" & u.time<=1L){
+		return(format.Date(x, format=output.format))
+	} else
+	
+	# Fractional time intervals – convert to smaller unit
+	if((trunc(u.time)-u.time)!=0){
+		if(sign(u.time)==1L){
+			while((trunc(u.time)-u.time)!=0){
+				if(unit=="sec"){stop("time interval must be an integer when converted to units of seconds")}
+				unit <- unit.choices[which.choice-1]
+				which.choice <- which(unit==unit.choices)
+				unit.cutoff <- switch(unit, sec=60, min=60, hour=24)
+				u.time <- unit.cutoff*u.time
+			}
+		}else{
+			stop("time interval must be positive")
+		}
+	} else 
+	
+	# Deal with case where units are days
+	if(unit=="day"){
+		if(u.time==1){
+			return(format.Date(trunc.POSIXt(x + 43200, units = units), format=output.format))
+		}else{
+			stop("units must be <= 1 day")
+		}
+	} else 
+	
+	# Deal w/ cases where time interval is 1 unit
+	if(u.time==1){
+			unit <- unit.choices[which.choice-1]
+			which.choice <- which(unit==unit.choices)
+			unit.cutoff <- switch(unit, sec=60, min=60, hour=24)
+			u.time <- unit.cutoff
+	} 
+	
+	# Deal with cases where time interval is > 1 of a larger unit
+	# Note that this follows up on case where u.time is > 1 and is not an integer
+	if(u.time>unit.cutoff){
+		u.time <- u.time%%unit.cutoff
+		mod.mess <- paste("Rounding to units =", u.time, unit) # may or may not want to make this a warning ...
+		warning(mod.mess)
+	}
+	
+	# =============================================
+	# = Convert to POSIX, or if can't, give error =
+	# =============================================
+	if(all(class(x)!="POSIX.ct")){
+		if(is.null(input.format)){
+			x <- as.POSIXct(x)
+		}else{
+			x <- as.POSIXct(x, format=input.format)
+		}
+	}
+	
+	# ===========================================================
+	# = Matching units (e.g., min) and unit multiples (e.g., 5) =
+	# ===========================================================
+	
+	which.choice <- which(unit==unit.choices)
+	form.unit <- c("%S", "%M", "%H", "%d")[which.choice]
+	mult <- as.integer(format.Date(x, format=form.unit))/u.time
+	after <- round(mult, 0)*u.time
+	# direction <- sign(after-before)
+	
+	# trunc.unit <- unit.choices[min(which.choice+1, length(unit.choices))]
+	trunc.unit <- unit.choices[min(which.choice+1, length(unit.choices))]
+	rounded <- trunc.POSIXt(x, trunc.unit) + switch(unit, sec = 1, min = 60, hour = 3600, day = 86400)*after
+	format.Date(rounded, format=output.format)
+}
+
+sondes <- list()
+for(i in 1:length(sites)){
+
+	t.site <- sites[i]
+	t.calInfo <- calInfo[calInfo[,"Site"]==t.site, ]
+	t.files <- t.calInfo[,"FileName"]
+
+	for(j in 1:nrow(t.calInfo)){
+
+		# Read in a file
+		t.f <- t.files[j]
+		t.fn <- paste("/Users/Battrd/Documents/School&Work/WiscResearch/Metabolism/WardSondes/",t.f,sep="")
+		t.dat <- read.table(t.fn, header=TRUE, sep=",")[-1,]
+		print(t.dat[1,])
+		# Format dates in file
+		if(t.f%in%c("Ward09Test.csv")){
+			t.date <- as.POSIXct(paste(t.dat[,"Date"], t.dat[,"Time"]), format="%m/%d/%y %H:%M")
+		}else{
+			t.date <- as.POSIXct(paste(t.dat[,"Date"], t.dat[,"Time"]), format="%m/%d/%y %H:%M:%S")
+		}
+		
+		t.year <- as.integer(format.Date(t.date, format="%Y"))
+		t.doy <- LakeMetabolizer:::date2doy(t.date)
+		
+		# Create a data.frame of sonde data
+		t.sonde0 <- data.frame(
+			year=t.year, 
+			date=t.date, 
+			doy=t.doy, 
+			doobs=a.nc(t.dat[,"DO.Conc"]), 
+			dosat=a.nc(t.dat[,"DO."]), 
+			wtr=a.nc(t.dat[,"Temp"])
+		)
+		
+		# Compute values needed to correct for drift
+		t.preCal <- mean(t.sonde0[1:t.calInfo[j,"Cal1"],"dosat"])
+		t.postCal <- mean(t.sonde0[t.calInfo[j,"Cal4"]:nrow(t.sonde0),"dosat"])
+		t.calSat <- c(t.preCal, t.postCal) # Average saturation before and after lake deployment
+		
+		t.lake.start <- t.calInfo[j, "Cal2"]
+		t.lake.stop <- t.calInfo[j, "Cal3"]
+		t.calTime <- c(t.sonde0[t.lake.start,"doy"], t.sonde0[t.lake.stop,"doy"]) # doy when initial cal ended, doy when final cal began
+		
+		t.sat.per.time <- diff(t.calSat)/diff(t.calTime) # average change in %sat (due to drift) per time (days)
+		
+		# Add NA's, drop days with too few observations, clip to in-lake observations, calculate drift
+		# t.sonde0.na1 <- LakeMetabolizer:::addNAs(t.sonde0[t.lake.start:t.lake.stop, ]) # KEEP! BELOW IS JUST TEST
+		t.sonde0.na1 <- LakeMetabolizer:::addNAs(t.sonde0[t.lake.start:t.lake.stop, ][-c(2240:2250),]) # TEST ONLY!
+		t.elapsed.doy <- c(0, cumsum(diff(t.sonde0.na1[,"doy"])))
+		t.cumul.drift.sat <- t.sat.per.time*t.elapsed.doy
+		t.cumul.drift.conc <- Sat2Conc(SDO2sat=t.cumul.drift.sat, SDtemp=t.sonde0.na1[,"wtr"])
+		
+		# Create a data frame that has drift-corrected oxygen
+		t.sonde0.na2 <- t.sonde0.na1
+		t.sonde0.na2[,"dosat"] <- t.sonde0.na1[,"dosat"] - t.cumul.drift.sat
+		t.sonde0.na2[,"doobs"] <- t.sonde0.na1[,"doobs"] - t.cumul.drift.conc
+		
+		t.sonde0.na2[,"date"] <- round.time(t.sonde0.na2[,"date"], "5 minutes")
+		
+		# t.sonde <- ddply(t.sonde0.na2, c("date", "year"), t.sonde.func) # too slow
+		
+		# only bother with duplicated rows
+		# t.sonde.func <- function(x){
+		# 	data.frame(doy=min(x[,"doy"], na.rm=TRUE), doobs=mean(x[,"doobs"]), dosat=mean(x[,"dosat"]), wtr=mean(x[,"wtr"]))
+		# }
+		# d1 <- duplicated(t.sonde0.na2[,"date"])
+		# d2 <- duplicated(t.sonde0.na2[,"date"], fromLast=TRUE)
+		# t.sonde.dups <- t.sonde0.na2[d1|d2,]
+		# t.sonde <- t.sonde0.na2
+		# t.sonde[d2&!d1,] <- ddply(t.sonde.dups, c("date", "year"), t.sonde.func)
+		
+		t.sonde <- t.sonde0.na2[!duplicated(t.sonde0.na2[,"date"]),]
+		
+		
+
+		
+		if(j==1){
+			sondes[[t.site]] <- t.sonde
+		}else{
+			sondes[[t.site]] <- rbind(sondes[[t.site]], t.sonde)
+		}
+		
+		 
+		
+		
+		
+	} # end looping through files pertaining to a given site
+		
+		
+} # end looping through sites
+		
+
+
+
+
+
+
+
