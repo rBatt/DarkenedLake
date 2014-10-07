16e0d5b7fd1d315519bcf214af01ef2232963f42
rbatt <battrd@gmail.com>
2014-10-07 08:54:56 -0400

    Remove ignored files

--- Scripts/toBrett_02Oct2014_d2H2O.R
+++
@@ -1,4 +0,0 @@
-
-library(plyr)
-DataRaw <- read.csv("/Users/Battrd/Documents/School&Work/WiscResearch/Isotopes_2012Analysis/Data/IsotopeData2012/WardIsotopes_2010&2012_17Jan2013.csv", header=TRUE)
-ddply(DataRaw, c("Year", "Habitat"), function(x){data.frame(muWater=mean(x[x[,"Type"]=="Water","dD"]))})
